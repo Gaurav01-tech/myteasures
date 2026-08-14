d9212e61b35b75445dcb4a5f79dfda42edf137a9
Gaurav01-tech <gauravrajs0205@gmail.com>
2026-08-14 22:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (18.89%) - LeetSync

--- tree/practice/source/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ tree/practice/source/1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-
-        int n = nums.size();
-        int max = INT_MAX;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] < max) {
-                max = nums[i];
-            }
+        int count = 0, n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1)%n ]) 
+                count++;
+            if (count > 1) 
+                return false;
         }
-        for(int k = 0; k < n; k++) {
-
-            if(nums[k] != max)
-                continue;
-            vector<int> temp = nums;
-            for(int i = 0; i < k; i++) {
-                int x = temp[0];
-                for(int j = 0; j < n - 1; j++) {
-                    temp[j] = temp[j + 1];
-                }
-                temp[n - 1] = x;
-            }
-            int flag = 1;
-            for(int i = 0; i < n - 1; i++) {
-                if(temp[i + 1] < temp[i]) {
-                    flag = 0;
-                    break;
-                }
-            }
-            if(flag == 1)
-                return true;
-        }
-        return false;
+        
+        return true;
     }
 };
